cecb5cad17419b54c41fd31381fb3dc13d66e6a5
jentlAntheunis <jentl.antheunis@student.howest.be>
2026-03-15 23:54:58 +0000

    added log flag

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -9,34 +9,60 @@ const Entity = {
 	Discovery: "discoveries",
 };
 
-async function post(entity, body) {
+async function post(entity, body, log = false) {
 	const res = await axios.post(BASE_URL + entity, body);
+	if (log) {
+		console.log("POST", BASE_URL + entity, body);
+	}
 	return res.data;
 }
 
-async function get(entity) {
+async function get(entity, log = false) {
 	const res = await axios.get(BASE_URL + entity);
+	if (log) {
+		console.log("GET", BASE_URL + entity);
+	}
 	return res.data;
 }
 
-async function getById(entity, id) {
+async function getById(entity, id, log = false) {
 	const res = await axios.get(BASE_URL + entity + "/" + id);
+	if (log) {
+		console.log("GET", BASE_URL + entity + "/" + id);
+	}
 	return res.data;
 }
 
-async function patch(entity, id, body) {
+async function patch(entity, id, body, log = false) {
 	const res = await axios.patch(BASE_URL + entity + "/" + id, body);
+	if (log) {
+		console.log("PATCH", BASE_URL + entity + "/" + id, body);
+	}
 	return res.data;
 }
 
-async function deleteById(entity, id) {
+async function deleteById(entity, id, log = false) {
 	const res = await axios.delete(BASE_URL + entity + "/" + id);
+	if (log) {
+		console.log("DELETE", BASE_URL + entity + "/" + id);
+	}
 	return res.data;
 }
 
-async function getUserByUsername(username) {
-	const res = await axios.get(BASE_URL + Entity.User + "/username/" + username);
-	return res.data;
+async function getUserByUsername(username, log = false) {
+	try {
+		const res = await axios.get(BASE_URL + Entity.User + "/username/" + encodeURIComponent(username));
+		if (log) {
+			console.log("GET", BASE_URL + Entity.User + "/username/" + encodeURIComponent(username));
+		}
+		return res.data;
+	} catch (error) {
+		if (axios.isAxiosError(error) && error.response?.status === 404) {
+			return null;
+		}
+
+		throw error;
+	}
 }
 
 export { Entity };
