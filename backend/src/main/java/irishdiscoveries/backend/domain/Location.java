e8c55de8dabd8b1054377770af6e8243d50d6226
jentlAntheunis <jentl.antheunis@student.howest.be>
2026-03-15 22:06:15 +0000

    cleaned up imports

--- backend/src/main/java/irishdiscoveries/backend/domain/Location.java
+++ backend/src/main/java/irishdiscoveries/backend/domain/Location.java
@@ -2,11 +2,7 @@ package irishdiscoveries.backend.domain;
 
 import java.util.UUID;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
