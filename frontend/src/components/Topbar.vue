28d7ab14189e77cc8279de60c7d82323724fa32d
jentlAntheunis <jentl.antheunis@student.howest.be>
2026-03-15 23:27:33 +0000

    styling

--- frontend/src/components/Topbar.vue
+++ frontend/src/components/Topbar.vue
@@ -1,20 +1,28 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
-  <div class="topbar__container">
-    <h1>Irish Discoveries</h1>
-  </div>
+	<div class="topbar__container">
+		<div class="topbar">
+			<h1>Irish Discoveries</h1>
+		</div>
+	</div>
 </template>
 
 <style scoped>
 .topbar__container {
-  height: 100%;
-  display: flex;
-  align-items: center;
+	height: 100%;
+	display: flex;
+	align-items: center;
+	background-color: var(--c-green);
 }
+
+.topbar {
+	max-width: 1000px;
+	margin: 0 auto;
+	flex-grow: 1;
+}
+
 h1 {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
 }
 </style>
