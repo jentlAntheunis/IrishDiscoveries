176184e6508eccadf84ad31eecf45b1e0c1af3d4
jentlAntheunis <jentl.antheunis@student.howest.be>
2026-03-24 10:48:41 +0000

    added logout button

--- frontend/src/components/Topbar.vue
+++ frontend/src/components/Topbar.vue
@@ -1,9 +1,24 @@
-<script setup></script>
+<script setup>
+import Button from "primevue/button";
+import { authState } from "@/state/auth.ts";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const logOut = () => {
+	authState.value = null;
+	localStorage.removeItem("auth");
+	router.push("/login");
+};
+</script>
 
 <template>
 	<div class="topbar__container">
 		<div class="topbar">
-			<h1>Irish Discoveries</h1>
+			<div class="topbar__content">
+				<h1>Irish Discoveries</h1>
+				<Button @click="logOut()">Log out</Button>
+			</div>
 		</div>
 	</div>
 </template>
@@ -20,6 +35,12 @@
 	max-width: 1000px;
 	margin: 0 auto;
 	flex-grow: 1;
+}
+
+.topbar__content {
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
 }
 
 h1 {
